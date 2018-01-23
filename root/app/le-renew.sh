0fb8ba0cdea030cca3107db170dd91444f5828b3
aptalca <aptalca@users.noreply.github.com>
2018-01-23 15:33:55 -0500

    fix renew script

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -2,15 +2,13 @@
 
 . /config/donoteditthisfile.conf
 
-if [ "$ORIGVALIDATION" = "dns" ]; then
-  PREFCHAL=""
-else
-  PREFCHAL='--pre-hook \"[[ ! -z $(ps -ef | grep nginx: | grep -v grep) ]] && s6-svc -d /var/run/s6/services/nginx\" --post-hook \"[[ ! -z $(ps -ef | grep \"s6-supervise nginx\" | grep -v grep) ]] && s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:\"'
-fi
-
 echo "<------------------------------------------------->"
 echo
 echo "<------------------------------------------------->"
 echo "cronjob running on "$(date)
 echo "Running certbot renew"
-certbot -n renew $PREFCHAL
+if [ "$ORIGVALIDATION" = "dns" ]; then
+  certbot -n renew
+else
+  certbot -n renew --pre-hook "[[ ! -z $(ps -ef | grep nginx: | grep -v grep) ]] && s6-svc -d /var/run/s6/services/nginx" --post-hook "[[ ! -z $(ps -ef | grep "s6-supervise nginx" | grep -v grep) ]] && s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+fi
