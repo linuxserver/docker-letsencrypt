017a0c829351883d105e27515e8509e2409cd85b
aptalca <aptalca@users.noreply.github.com>
2018-04-07 21:59:54 -0400

    fix renewal hooks

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -10,5 +10,5 @@ echo "Running certbot renew"
 if [ "$ORIGVALIDATION" = "dns" ]; then
   certbot -n renew --post-hook "cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
 else
-  certbot -n renew --pre-hook "[[ ! -z $(ps -ef | grep nginx: | grep -v grep) ]] && s6-svc -d /var/run/s6/services/nginx" --post-hook "[[ ! -z $(ps -ef | grep "s6-supervise nginx" | grep -v grep) ]] && s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+  certbot -n renew --pre-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -d /var/run/s6/services/nginx; fi" --post-hook "if ps aux | grep 's6-supervise nginx' | grep -v grep > /dev/null; then s6-svc -u /var/run/s6/services/nginx; fi; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
 fi
