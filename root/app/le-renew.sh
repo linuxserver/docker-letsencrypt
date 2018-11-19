614457a3e418e4c13e0beae4197f8de79238ac75
Eric Nemchik <eric@nemchik.com>
2018-11-19 10:21:47 -0600

    Adjust formatting to use shfmt and pass shellcheck

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/with-contenv bash
+# shellcheck disable=SC1008
 
+# shellcheck disable=SC1091
 . /config/donoteditthisfile.conf
 
 echo "<------------------------------------------------->"
 echo
 echo "<------------------------------------------------->"
-echo "cronjob running on "$(date)
+echo "cronjob running on $(date)"
 echo "Running certbot renew"
 if [ "$ORIGVALIDATION" = "dns" ] || [ "$ORIGVALIDATION" = "duckdns" ]; then
-  certbot -n renew \
+    certbot -n renew \
     --post-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -h /var/run/s6/services/nginx; fi; \
     cd /config/keys/letsencrypt && \
     openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass: && \
     sleep 1 && \
     cat {privkey,fullchain}.pem > priv-fullchain-bundle.pem"
 else
-  certbot -n renew \
+    certbot -n renew \
     --pre-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -d /var/run/s6/services/nginx; fi" \
     --post-hook "if ps aux | grep 's6-supervise nginx' | grep -v grep > /dev/null; then s6-svc -u /var/run/s6/services/nginx; fi; \
     cd /config/keys/letsencrypt && \
