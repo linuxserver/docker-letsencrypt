452453a135b77a8de1141b6db9ee793030bc5434
Eric Nemchik <eric@nemchik.com>
2019-03-04 14:11:36 -0600

    Output to /dev/null

--- root/healthcheck.sh
+++ root/healthcheck.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # universal check that the process is running
-pgrep -af nginx
+pgrep -af nginx > /dev/null
 
 # individualized check(s) specific to the app
-nginx -tq -c /config/nginx/nginx.conf
+nginx -tq -c /config/nginx/nginx.conf > /dev/null
