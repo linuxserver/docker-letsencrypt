0c6bdd5346442f312d200340931c6e8c6196b363
Eric Nemchik <eric@nemchik.com>
2019-03-04 13:48:08 -0600

    Fix ShellCheck SC2009

--- root/healthcheck.sh
+++ root/healthcheck.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # universal check that the process is running
-ps -ef | grep -v 'grep --color=auto' | grep nginx
-ps -ef | grep '^abc.*nginx'
+pgrep -af nginx
 
 # individualized check(s) specific to the app
 nginx -tq -c /config/nginx/nginx.conf
